164e545904e320a1c5df1717b397d53ff9cdc015
Rafael Sampaio <rafaelsampaio200802@gmail.com>
2022-01-27 22:55:45 -0300

    Aula 3

--- pages/chat.js
+++ pages/chat.js
@@ -1,7 +1,211 @@
-export default function chatPage(){
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import react from 'react';
+import React from 'react';
+import appConfig from '../config.json';
+
+export default function ChatPage() {
+    // Sua lógica vai aqui
+    {/*
+        =Usuario
+            - Usuario digita no campo texto
+            - Apertar enter para enviar
+            - Tem que add o texto na listagem
+            
+        =DEV
+            -[X] Campos Criados
+            -[X] Vamos Usar onChange e useState para a variavel de texto
+            -[X] Lista de msg
+    */}
+    // ./Sua lógica vai aqui
+
+    function handleNovaMensagem(novaMensagem) {
+        const mensagem = {
+            id: listaDeMensagens.length,
+            de: 'RafaCS2002',
+            texto: novaMensagem
+        };
+        setListaDeMensagens([
+            mensagem,
+            ...listaDeMensagens,
+        ]);
+        setMensagem("");
+    }
+
+    const [mensagem, setMensagem] = React.useState("");
+    const [listaDeMensagens, setListaDeMensagens] = react.useState([]);
+
+
     return (
-        <div>
-            <h2>Pagina do Chat</h2>    
-        </div>
+        <Box
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundColor: appConfig.theme.colors.primary["400"],
+                backgroundImage: 'url(https://www.tbsconsultoria.com.br/wp-content/uploads/2021/05/fantasy-universe-space-background-volumetric-lighting-3d-render-1-scaled.jpg)',
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                color: appConfig.theme.colors.neutrals['000']
+            }}
+        >
+            <Box
+                styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    borderRadius: '5px',
+                    backgroundColor: appConfig.theme.colors.neutrals[700]+"80",
+                    height: '100%',
+                    maxWidth: '95%',
+                    maxHeight: '95vh',
+                    padding: '32px',
+                }}
+            >
+                <Header />
+                <Box
+                    styleSheet={{
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: appConfig.theme.colors.neutrals[600]+'80',
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
+                    }}
+                >
+                    {/* 
+                    lista De Mensagens: {listaDeMensagens.map((mensagemAtual)=>{
+                                                console.log(mensagemAtual)
+                                                return(
+                                                    <li key={mensagemAtual.id}>
+                                                        {mensagemAtual.texto}
+                                                    </li>
+                                                )
+                                            })
+                                        } */}
+
+                    <MessageList mensagens={listaDeMensagens} />
+                    {/* <MessageList mensagens={[]}/> */}
+
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <TextField
+                            value={mensagem}
+                            onChange={(event) => {
+                                const valor = event.target.value;
+                                setMensagem(valor);
+                            }}
+                            onKeyPress={(event) => {
+                                console.log(event)
+                                if (event.key == 'Enter' && mensagem != '') {
+                                    handleNovaMensagem(mensagem);
+                                    event.preventDefault();
+                                }
+                            }}
+                            placeholder="Insira sua mensagem aqui..."
+                            type="textarea"
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '6px 8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                marginRight: '12px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+function Header() {
+    return (
+        <>
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
+function MessageList(props) {
+    console.log('MessageList', props);
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflow: 'scroll',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
+            {props.mensagens.map((mensagemAtual) => {
+                return (
+                    <Text
+                        key={mensagemAtual.id}
+                        tag="li"
+                        styleSheet={{
+                            borderRadius: '5px',
+                            padding: '6px',
+                            marginBottom: '12px',
+                            hover: {
+                                backgroundColor: appConfig.theme.colors.neutrals[700],
+                            }
+                        }}
+                    >
+                        <Box
+                            styleSheet={{
+                                marginBottom: '8px',
+                            }}
+                        >
+                            <Image
+                                styleSheet={{
+                                    width: '20px',
+                                    height: '20px',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    marginRight: '8px',
+                                }}
+                                src={`https://github.com/RafaCS2002.png`}
+                            />
+                            <Text tag="strong">
+                                {mensagemAtual.de}
+                            </Text>
+                            <Text
+                                styleSheet={{
+                                    fontSize: '10px',
+                                    marginLeft: '8px',
+                                    color: appConfig.theme.colors.neutrals[300],
+                                }}
+                                tag="span"
+                            >
+                                {(new Date().toLocaleDateString())}
+                            </Text>
+                        </Box>
+                        {mensagemAtual.texto}
+                    </Text>
+                )
+            })}
+
+        </Box>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,23 @@
-import { Box, Button, Text, TextField, Image} from '@skynexui/components';
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 
-function Titulo(props){
-    console.log(props.children);
-    const Tag = props.tag;
-    return(
-        <>
-            <Tag>{props.children}</Tag>
-            <style jsx>{`
+function Titulo(props) {
+  const Tag = props.tag;
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
                 ${Tag} {
                     color: ${appConfig.theme.colors.neutrals['000']};
                     font-size:24px;
                     font-weigth:600;
                 }
             `}</style>
-        </>
-    );
+    </>
+  );
 }
 
 // //Componente React
@@ -35,57 +34,56 @@ function Titulo(props){
 //   export default HomePage
 
 export default function PaginaInicial() {
-    //const username = 'omariosouto';
-    const [username, setUsername] = React.useState('');
-    const roteamento = useRouter();
+  //const username = 'omariosouto';
+  const [img, setImg] = React.useState(`https://www.ecp.org.br/wp-content/uploads/2017/12/default-avatar-1.png`);
+  const [username, setUsername] = React.useState('');
+  const roteamento = useRouter();
 
-    console.log(roteamento);
+  return (
+    <>
 
-    return (
-      <>
-        
+      <Box
+        styleSheet={{
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          backgroundColor: appConfig.theme.colors.primary[300],
+          backgroundImage: 'url(https://www.tbsconsultoria.com.br/wp-content/uploads/2021/05/fantasy-universe-space-background-volumetric-lighting-3d-render-1-scaled.jpg)',
+          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+        }}
+      >
         <Box
           styleSheet={{
-            display: 'flex', alignItems: 'center', justifyContent: 'center',
-            backgroundColor: appConfig.theme.colors.primary[300],
-            backgroundImage: 'url(https://ogimg.infoglobo.com.br/in/24581416-aae-bd5/FT1086A/THEROCK.jpg)',
-            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+            display: 'flex',
+            al1Items: 'center',
+            justifyContent: 'space-between',
+            flexDirection: {
+              xs: 'column',
+              sm: 'row',
+            },
+            width: '100%', maxWidth: '700px',
+            borderRadius: '5px', padding: '32px', margin: '16px',
+            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+            backgroundColor: appConfig.theme.colors.neutrals[999]+'70',
           }}
         >
+          {/* Formulário */}
           <Box
+            as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log("Alguem submeteu");
+              roteamento.push('/chat');
+            }}
             styleSheet={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              flexDirection: {
-                xs: 'column',
-                sm: 'row',
-              },
-              width: '100%', maxWidth: '700px',
-              borderRadius: '5px', padding: '32px', margin: '16px',
-              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-              backgroundColor: appConfig.theme.colors.neutrals[999],
+              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            {/* Formulário */}
-            <Box
-              as="form"
-              onSubmit={function (infosDoEvento){
-                infosDoEvento.preventDefault();
-                console.log("Alguem submeteu");
-                roteamento.push('/chat');
-              }}
-              styleSheet={{
-                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
-              }}
-            >
-              <Titulo tag="h2">Calderão do Samps</Titulo>
-              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
-                {appConfig.name}
-              </Text>
-  
-              {/* <input  type="text" 
+            <Titulo tag="h2">Calderão do Samps</Titulo>
+            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+              {appConfig.name}
+            </Text>
+
+            {/* <input  type="text" 
                       value={username}
                       onChange={function handler(event){
                         console.log("usuario digitou", event.target.value)
@@ -93,76 +91,81 @@ export default function PaginaInicial() {
                         setUsername(valor);
                       }}
                       /> */}
-              <TextField
-                fullWidth
-                textFieldColors={{
-                  neutral: {
-                    textColor: appConfig.theme.colors.neutrals[200],
-                    mainColor: appConfig.theme.colors.neutrals[900],
-                    mainColorHighlight: appConfig.theme.colors.primary[500],
-                    backgroundColor: appConfig.theme.colors.neutrals[800],
-                  },
-                }}
-                value={username}
-                onChange={function handler(event){
-                  console.log("usuario digitou", event.target.value)
-                  const valor = event.target.value;
-                  setUsername(valor);
-                }}
-              />
-              <Button
-                type='submit'
-                label='Entrar'
-                fullWidth
-                buttonColors={{
-                  contrastColor: appConfig.theme.colors.neutrals["000"],
-                  mainColor: appConfig.theme.colors.primary[500],
-                  mainColorLight: appConfig.theme.colors.primary[400],
-                  mainColorStrong: appConfig.theme.colors.primary[600],
-                }}
-              />
-            </Box>
-            {/* Formulário */}
-  
-  
-            {/* Photo Area */}
-            <Box
+            <TextField
+              fullWidth
+              textFieldColors={{
+                neutral: {
+                  textColor: appConfig.theme.colors.neutrals[200],
+                  mainColor: appConfig.theme.colors.neutrals[900],
+                  mainColorHighlight: appConfig.theme.colors.primary[500],
+                  backgroundColor: appConfig.theme.colors.neutrals[800],
+                },
+              }}
+              value={username}
+              onChange={function handler(event) {
+                const valor = event.target.value;
+                setUsername(valor);
+                if (valor != '') {
+                  setImg(`https://github.com/${valor}.png`);
+                } else {
+                  setImg(`https://www.ecp.org.br/wp-content/uploads/2017/12/default-avatar-1.png`);
+                }
+
+              }}
+            />
+            <Button
+              type='submit'
+              label='Entrar'
+              fullWidth
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals["000"],
+                mainColor: appConfig.theme.colors.primary[500],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[600],
+              }}
+            />
+          </Box>
+          {/* Formulário */}
+
+
+          {/* Photo Area */}
+          <Box
+            styleSheet={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              maxWidth: '200px',
+              padding: '16px',
+              backgroundColor: appConfig.theme.colors.neutrals[800],
+              border: '1px solid',
+              borderColor: appConfig.theme.colors.neutrals[999],
+              borderRadius: '10px',
+              flex: 1,
+              minHeight: '240px',
+            }}
+          >
+            <Image
               styleSheet={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                maxWidth: '200px',
-                padding: '16px',
-                backgroundColor: appConfig.theme.colors.neutrals[800],
-                border: '1px solid',
-                borderColor: appConfig.theme.colors.neutrals[999],
-                borderRadius: '10px',
-                flex: 1,
-                minHeight: '240px',
+                borderRadius: '50%',
+                marginBottom: '16px',
+              }}
+              src={img}
+            />
+            <Text
+              variant="body4"
+              styleSheet={{
+                color: appConfig.theme.colors.neutrals[200],
+                backgroundColor: appConfig.theme.colors.neutrals[900],
+                padding: '3px 10px',
+                borderRadius: '1000px'
               }}
             >
-              <Image
-                styleSheet={{
-                  borderRadius: '50%',
-                  marginBottom: '16px',
-                }}
-                src={`https://github.com/${username}.png`}
-              />
-              <Text
-                variant="body4"
-                styleSheet={{
-                  color: appConfig.theme.colors.neutrals[200],
-                  backgroundColor: appConfig.theme.colors.neutrals[900],
-                  padding: '3px 10px',
-                  borderRadius: '1000px'
-                }}
-              >
-                {username}
-              </Text>
-            </Box>
-            {/* Photo Area */}
+              {username}
+            </Text>
           </Box>
+          {/* Photo Area */}
         </Box>
-      </>
-    );
-  }
+      </Box>
+    </>
+  );
+}
